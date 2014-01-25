3e5bad3e15692b708e9ac5107bccb07f64e29483
Kristoffer Jetmundsen <krisjet@gmail.com>
2014-01-25 15:15:45 +0100

    Movement and camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate (){
+		var player = GameObject.FindGameObjectWithTag ("Player");
+		transform.position = new Vector3 (transform.position.x - (transform.position.x - player.transform.position.x)/10, transform.position.y - (transform.position.y - player.transform.position.y)/10, -10);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Camera myCamera;
+	public float h = 2;
+	public float maxSpeed = 1.2f;
+	public Sprite[] mySprite;
+	string direction = "right";
+	private int blinkLength = 5;
+	private bool blink = true;
+
+	// Update is called once per frame
+	void Update () {
+		//Movement();
+	}
+
+	void FixedUpdate () 
+    {
+    	Movement();
+		MoveCamera();
+    }
+
+	void MoveCamera(){
+		int amount = 2;
+		//myCamera.transform.position = transform.position;
+		/*
+		int xNudge = 0;
+		if (xDirection == "right") {
+			xNudge = amount;
+		} else if (xDirection == "left") {
+			xNudge = -amount;
+		} else {
+			xNudge = 0;
+		}
+		int yNudge = 0;
+		if (yDirection == "up") {
+			yNudge = amount;
+		} else if (yDirection == "down") {
+			yNudge = -amount;
+		} else {
+
+			yNudge = 0;
+		}
+		myCamera.transform.position = new Vector2(this.transform.position.x + xNudge, this.transform.position.y + yNudge);
+*/
+	}
+
+	void Movement () {
+		if(Input.GetKey( KeyCode.D )){
+			
+			rigidbody2D.velocity = new Vector2(h, rigidbody2D.velocity.y);
+						
+			transform.eulerAngles = new Vector2(0, 0);
+			GetComponent<SpriteRenderer>().sprite = mySprite[2];
+			direction = "right";
+		} else  if(Input.GetKey( KeyCode.A )){
+			
+			rigidbody2D.velocity = new Vector2(-h, rigidbody2D.velocity.y);
+			transform.eulerAngles = new Vector2(0, 180);
+			GetComponent<SpriteRenderer>().sprite = mySprite[2];
+			direction = "left";
+		} else {
+
+			rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+		}
+
+		if(Input.GetKey( KeyCode.S )){
+
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, -h);
+			GetComponent<SpriteRenderer>().sprite = mySprite[0];
+			direction = "down";
+
+		} else if(Input.GetKey( KeyCode.W )){
+
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, h);
+			GetComponent<SpriteRenderer>().sprite = mySprite[1];
+			direction = "up";
+		} else {
+
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.E) && blink) { 
+			if(direction == "up"){
+				transform.position = new Vector2(transform.position.x, transform.position.y + blinkLength);
+			} else if(direction == "down"){
+				transform.position = new Vector2(transform.position.x, transform.position.y - blinkLength);
+			} else if(direction == "right"){
+				transform.position = new Vector2(transform.position.x + blinkLength, transform.position.y);
+			} else if(direction == "left"){
+				transform.position = new Vector2(transform.position.x - blinkLength, transform.position.y);
+			}
+		}
+	
+	}
+}
